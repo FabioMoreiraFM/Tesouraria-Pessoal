598fd7ec8be3348b05689ba5f51db4191f8eb5ef
Fabio Moreira <fabio.moreira@dataa.com.br>
2021-04-05 16:28:18 -0300

    Finaliza tela de login/cadastro de usuário

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import './App.css';
+import React, { Component } from 'react'
+import {Route, Switch, Redirect} from 'react-router-dom'
 
-function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+import Auth from './containers/Auth/Auth'
+
+import styles from './App.module.css'
+
+class App extends Component {
+  render () {
+    let routes = (
+      <Switch>
+        <Route path="/" component={Auth} />
+        <Redirect to="/" />
+      </Switch>        
+    )
+
+    return (
+      <div className={styles.App}>
+        {routes}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,150 @@
+import React from 'react'
+
+import Input from "@material-ui/core/OutlinedInput";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import Button from '@material-ui/core/Button';
+import CopyrightIcon from '@material-ui/icons/Copyright';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import PersonIcon from '@material-ui/icons/Person';
+import LockIcon from '@material-ui/icons/Lock';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import IconButton from '@material-ui/core/IconButton';
+
+import reactIcon from './../../assets/react.png'
+
+import styles from './Auth.module.css'
+import { makeStyles } from '@material-ui/core';
+
+const useStylesIcon = makeStyles(theme => ({
+    root: {
+        color: 'grey'
+    }
+}))
+
+const useStylesInput = makeStyles(theme => ({
+    root: {
+        width: '330px',
+        height: '41px',
+        borderRadius: 100, 
+        fontSize: '15px',
+        border: '1px solid',
+        borderColor: '#b8b6b6',
+        boxShadow: '0 3px #e3e1e1',
+        fontFamily: 'Bahnschrift'
+    }
+}))
+
+const useStylesButton = makeStyles(theme => ({
+    root: {
+        backgroundColor: 'black',
+        opacity: 0.84,
+        fontSize: '20px',
+        color: 'white',
+        borderRadius: 100,        
+        width: '330px',
+        '&:hover': {
+            backgroundColor: 'grey'
+        }        
+    }
+}))
+
+const useStylesButtonText = makeStyles(theme => ({
+    root: {
+        textTransform: 'none'    
+    }
+}))
+
+const Auth = props => {
+    const [showPassword, setShowPassword] = React.useState(false)
+    const [isSignIn, setIsSignIn] = React.useState(true)
+
+    const classes = useStylesIcon();
+    const classesInput = useStylesInput();
+    const classesButton = useStylesButton();
+    const classesButtonText = useStylesButtonText();
+    
+    const handleClickShowPassword = () => {
+        setShowPassword(!showPassword);
+    };
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
+
+    let repeatPasswordField = (
+        <Input classes={{root: classesInput.root}} variant="outlined" placeholder={"Repetir Nova Senha"} type={showPassword ? 'text' : 'password'} 
+        startAdornment={
+            <InputAdornment classes={{root: classes.root}} position="start">
+                <LockIcon />
+            </InputAdornment>
+        }
+        endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+              >
+                {showPassword ? <Visibility /> : <VisibilityOff />}
+              </IconButton>
+            </InputAdornment>
+        }
+        />
+    )
+
+    return (
+        <div className={styles.Auth}>
+            <div className={styles.FormDiv}>
+                <div className={styles.FormHeader}>
+                    <span>{isSignIn ? 'Acesse Agora!' : 'Cadastre-se!'}</span>
+                </div>
+
+                <div className={styles.InputContainer}>
+                    <Input classes={{root: classesInput.root}} variant="outlined" placeholder="E-mail" 
+                            startAdornment={
+                                <InputAdornment classes={{root: classes.root}} position="start">
+                                    <PersonIcon />
+                                </InputAdornment>
+                            }                            
+                    />
+
+                    <Input classes={{root: classesInput.root}} variant="outlined" placeholder={isSignIn ? "Senha" : "Nova Senha"} type={showPassword ? 'text' : 'password'} 
+                            startAdornment={
+                                <InputAdornment classes={{root: classes.root}} position="start">
+                                    <LockIcon />
+                                </InputAdornment>
+                            }
+                            endAdornment={
+                                <InputAdornment position="end">
+                                  <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                  >
+                                    {showPassword ? <Visibility /> : <VisibilityOff />}
+                                  </IconButton>
+                                </InputAdornment>
+                            }
+                    />
+
+                    {isSignIn ? null : repeatPasswordField}
+
+                </div>
+                <div>
+                    <Button variant="contained" classes={{root: classesButton.root}}>
+                        {isSignIn ? 'Entrar' : 'Cadastrar'}
+                    </Button>
+                </div>                    
+                <div className={styles.FormFooter}>
+                    <span>{isSignIn ? 'Não possui uma conta?' : 'Já possui uma conta?'}</span><Button onClick={() => setIsSignIn(!isSignIn)} color="primary" classes={{root: classesButtonText.root}} >Clique aqui.</Button>
+                </div>
+            </div>
+            <div className={styles.Copyright}>
+                <CopyrightIcon className={styles.Icon}/> Fabio Moreira, todos os direitos reservados. Feito com <FavoriteIcon className={styles.HeartIcon}/> e um pouco de <img className={styles.ReactIcon} src={reactIcon} alt="React Icon"/>.                
+            </div>
+        </div>
+    )
+}
+
+export default Auth;
