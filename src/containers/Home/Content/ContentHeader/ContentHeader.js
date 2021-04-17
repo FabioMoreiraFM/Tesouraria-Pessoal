b1378853560f214cb300e0fc1ea05065c425d03c
Fabio Moreira <fabio.moreira@dataa.com.br>
2021-04-17 10:41:26 -0300

    Adiciona botao de notificacoes e quadro com as notificacoes

--- src/containers/Home/Content/ContentHeader/ContentHeader.js
+++ src/containers/Home/Content/ContentHeader/ContentHeader.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
 import styles from './ContentHeader.module.css'
+import NotificationsIcon from '@material-ui/icons/Notifications';
 
 const ContentHeader = (props) =>  {
+    const [showNotificationList, setShowNotificationList] = React.useState(false);
+    
     return (
         <header className={styles.ContentHeader}>
             <div className={styles.IconSquare}>
@@ -11,6 +14,21 @@ const ContentHeader = (props) =>  {
             <div className={styles.Title}>
                 <span>{props.title}</span>
                 <span>{props.subtitle}</span>
+            </div>
+            <div className={styles.HeaderIcons} onMouseOver={() => setShowNotificationList(true)} onMouseOut={() => setShowNotificationList(false)} >
+                <NotificationsIcon />
+                { showNotificationList ?
+                <div className={styles.Notifications}>
+                    <span>Sua dívida "IPTU" vencerá amanhã (04/04/2021).</span>
+                    <hr/>
+                    <span>
+                        Sua dívida "Seguro" venceu ontem (01/01/2021). 
+                        Regularize a situação e evite juros e multa!
+                    </span>
+                    <hr/>
+                    <span>Sua dívida "Seguro" vencerá amanhã (01/01/2021).</span>
+                </div>
+                : null }
             </div>
         </header>
     )
